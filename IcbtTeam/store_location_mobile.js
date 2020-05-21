d29b99e1752d99b00616fcc383e42e50b7aa4e89
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-05-21 16:14:34 +0800

    手机版location修改

--- IcbtTeam/store_location_mobile.js
+++ IcbtTeam/store_location_mobile.js
@@ -3,3 +3,18 @@ AOS.init({
     duration: 1000,
 });
 
+var button_more = document.querySelectorAll('button a');
+var div_block0 = document.querySelector('.box1_display');
+var div_block1 = document.querySelector('.box2_display');
+var div_block2 = document.querySelector('.box3_display');
+
+for (let i = 0; i < button_more.length; i++) {
+
+    var button = button_more;
+    button_more[i].addEventListener("click", () => {
+        var number = i + 1;
+        $('.box' + number + '_display').attr("style", "display: block !important");
+        $('.box' + number + '_display').attr("style", "display: flex !important");
+    });
+
+}
